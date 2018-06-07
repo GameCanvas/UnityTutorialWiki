337aaf9e44d25426e3fa6dae8f82122e8579df5a
rabbitbooster <yusuke.kikuchi@gmail.com>
2018-06-08 00:21:55 +0900

    ボーリングチュートリアル追加

---
+++ UnityTutorialWiki/Bouring/Assets/Scripts/PinCounter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PinCounter : MonoBehaviour
+{
+    public int Counter;
+    public Text text;
+    int count;
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = Counter.ToString();
+        count = 0;
+    }
+}
