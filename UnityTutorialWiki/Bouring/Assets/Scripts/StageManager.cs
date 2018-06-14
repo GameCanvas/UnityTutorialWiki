49f15e52259325b3a9dd4ba659713475f6ed5a4d
yusuke-master-laptop <yusuke.kikuchi@outlook.com>
2018-06-14 23:12:13 +0900

    ボーリングスクリプト講座の完成！

---
+++ UnityTutorialWiki/Bouring/Assets/Scripts/StageManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    public Image[] StageClearImage;
+    private Rigidbody rigid;
+    private object pinCounter;
+    private Type PinCounterType;
+    void Start()
+    {
+        var ball = GameObject.Find("Ball");
+        if (ball != null)
+        {
+            rigid = ball.GetComponent<Rigidbody>();
+        }
+        var pins = GameObject.Find("Pins");
+        if (ball != null)
+        {
+            PinCounterType = Type.GetType("PinCounter");
+            if (PinCounterType != null)
+            {
+                pinCounter = pins.GetComponent(PinCounterType);
+            }
+
+        }
+
+
+    }
+    private void Update()
+    {
+        if (rigid != null && Mathf.Abs(rigid.velocity.x) > 1f)
+        {
+            StageClear(1);
+        }
+
+        if (PinCounterType != null && (int)PinCounterType.GetField("Counter").GetValue(pinCounter) > 1)
+        {
+            StageClear(2);
+        }
+
+    }
+    public void StageClear(int index)
+    {
+        StageClearImage[index - 1].enabled = true;
+    }
+
+}
